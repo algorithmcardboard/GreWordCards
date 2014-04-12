6564d25b13c3215b5ea1ab778d053aef79927ed9
Anirudhan Jegannathan <rajegannathan@gmail.com>
2014-04-12 12:49:58 +0530

    removing fragment_home.xml from repo

--- src/in/rajegannathan/grewordcards/HomeActivity.java
+++ src/in/rajegannathan/grewordcards/HomeActivity.java
@@ -6,16 +6,13 @@ import in.rajegannathan.grewordcards.localdb.DBHelper;
 import java.util.logging.Logger;
 
 import android.app.Activity;
-import android.app.Fragment;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.EditText;
 
 public class HomeActivity extends Activity {
@@ -70,22 +67,4 @@ public class HomeActivity extends Activity {
 		db.insert(Wordcard.TABLE_NAME, null, values);
 		logger.info("word inserted successfully");
 	}
-
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_home, container,
-					false);
-			return rootView;
-		}
-	}
-
 }
