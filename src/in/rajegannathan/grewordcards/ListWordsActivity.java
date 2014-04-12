9d61a223a15b8b08590a76fba56fe1feb709a5c2
Anirudhan Jegannathan <rajegannathan@gmail.com>
2014-04-12 22:34:18 +0530

    minor changes for longpress

--- src/in/rajegannathan/grewordcards/ListWordsActivity.java
+++ src/in/rajegannathan/grewordcards/ListWordsActivity.java
@@ -12,9 +12,13 @@ import android.os.Bundle;
 import android.provider.BaseColumns;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 
-public class ListWordsActivity extends ListActivity{
+public class ListWordsActivity extends ListActivity {
 
 	private static final Logger logger = Logger
 			.getLogger(ListWordsActivity.class.getName());
@@ -22,23 +26,40 @@ public class ListWordsActivity extends ListActivity{
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		// setContentView(R.layout.activity_list_words);
-		
+//		setContentView(R.layout.activity_list_words);
+
 		DBHelper mDbHelper = new DBHelper(getApplicationContext());
 		SQLiteDatabase db = mDbHelper.getReadableDatabase();
 
 		String[] fromColumns = { Wordcard.COLUMN_WORD };
 		int[] toViews = { R.id.singleWord };
 
-		String[] projection = { BaseColumns._ID, Wordcard.COLUMN_WORD, Wordcard.COLUMN_VIEWS };
+		String[] projection = { BaseColumns._ID, Wordcard.COLUMN_WORD,
+				Wordcard.COLUMN_VIEWS };
 		String sortOrder = Wordcard.COLUMN_VIEWS + " DESC";
 		Cursor cursor = db.query(Wordcard.TABLE_NAME, projection, null, null,
 				null, null, sortOrder);
-		;
 		SimpleCursorAdapter mAdapter = new SimpleCursorAdapter(this,
 				R.layout.fragment_list_words, cursor, fromColumns, toViews, 0);
 
 		setListAdapter(mAdapter);
+
+		ListView wordList = getListView();
+		if (wordList != null) {
+			wordList.setOnItemLongClickListener(new OnItemLongClickListener() {
+
+				@Override
+				public boolean onItemLongClick(AdapterView<?> parent,
+						View view, int position, long id) {
+					logger.info("longPress on item. Position is "+position+" id is "+id);
+					return false;
+				}
+			});
+		} else {
+			logger.info("wordList is empty");
+			ListView listView = getListView();
+			logger.info("listview is "+listView.toString());
+		}
 	}
 
 	@Override
